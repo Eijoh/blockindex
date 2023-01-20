4ba87f6e8295069150dbdc46d8885ea952ce0b99
Eijoh <61121271+Eijoh@users.noreply.github.com>
2023-01-20 21:29:23 +0100

    Update nsw.user.js

--- nsw.user.js
+++ nsw.user.js
@@ -1,17 +1,27 @@
 // ==UserScript==
-// @name     NoAds (NSW)
+// @name     NoAds (NSW) ⭐
 // @include  https://ouo.io/*
 // @include  https://ouo.press/*
 // @include  https://nxbrew.com/*
+// @include  https://cutdl.xyz/*
 // @version  1.0
 // @author   Hipelago
+// @run-at   document-end
+// @grant    none
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
 /*
 */
-
+(function() {
+  var si = setInterval;
+  setInterval = function(fn, duration) {
+    if (duration >= 1000) duration = duration === 1000 ? 500 : duration;
+    return si.apply(this, arguments);
+  };
+})();
 setInterval(function () {
     document.getElementsByClassName("btn btn-main")[0].click();
-}, 500);
+}, 1000);
 var links,thisLink;
 links = document.evaluate("//a[@href]",
     document,
@@ -23,5 +33,9 @@ for (var i=0;i<links.snapshotLength;i++) {
     thisLink.href = thisLink.href.replace('http://1link.club/',
                                           'https://1link.club/');
     thisLink.href = thisLink.href.replace('https://1link.club/',
-                                          'https://yes.noads.ga/ER.php?!VAR!000!=https://1link.club/');
+                                          'http://yes.noads.ga/ER.php?!VAR!000!=https://1link.club/');
 }
+$(".btn.btn-success.btn-lg.get-link.disabled").hide()
+setInterval(function () {
+    document.getElementsByClassName("btn btn-success btn-lg get-link")[0].click();
+}, 2000);
